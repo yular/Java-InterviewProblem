abe9c68b6b68587de1d6586fa27818a6490259a8
Baichuan Yang <1062969706@qq.com>
2018-03-13 00:15:02 -0700

    Create lintcode_lowercase-to-uppercase.java

---
+++ LintCode/lintcode_lowercase-to-uppercase.java
@@ -0,0 +1,15 @@
+/*
+*
+* Tag: Implementation
+* Time: O(1)
+* Space: O(1)
+*/
+public class Solution {
+    /**
+     * @param character: a character
+     * @return: a character
+     */
+    public char lowercaseToUppercase(char character) {
+        return Character.toUpperCase(character);
+    }
+}
